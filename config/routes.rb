e7513ba190f06cfd17fa03bd8d980a513335cb99
mihaneyano <131777676+mihaneyano@users.noreply.github.com>
2023-05-12 07:40:57 +0000

    [finish]4-9

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-  get 'homes/about' => 'homes#about', as: 'about'
+
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
+
+  get '/homes/about' => 'homes#about', as: 'about'
 end
